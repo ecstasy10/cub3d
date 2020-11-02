45ac5a5957f2a5cc7280e499597f4d6bf6f1ba29
itsnotLonee <david.balboa.rojo@gmail.com>
2020-11-02 14:12:48 +0100

    huge update

--- cub3d.h
+++ cub3d.h
@@ -1,67 +1,8 @@
-#include "mlxopengl/mlx.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
+//
+// Created by Dev on 17/10/2020.
+//
 
-#define WHITE 0x00FFFFFF
-#define BLACK 0x00000000
-#define RED 0x00FF0000
-#define GREEN 0x0000FF00
-#define BLUE 0x000000FF
-#define MAGENTA 0x00FF00FF
-#define YELLOW 0x00FFFF00
-#define CYAN 0x0000FFFF
-#define SKY 0x00CDF9FF
-#define WALL 0x00666666
-#define DIRT 0x00E0AC69
+#ifndef CUB3D_CUB3D_H
+#define CUB3D_CUB3D_H
 
-typedef struct	s_mlx
-{
-	void			*ptr;
-}				t_mlx;
-
-typedef struct	s_win
-{
-	void			*ptr;
-	int				x;
-	int				y;
-}				t_win;
-
-typedef struct	s_img
-{
-	void			*ptr;
-	unsigned int	*adr;
-	int				bpp;
-	int				sl;
-	int				end;
-}				t_img;
-
-typedef struct	s_map
-{
-	int				tab[10][10];
-	int				x;
-	int				y;
-}				t_map;
-
-typedef struct	s_pos
-{
-	double			x;
-	double			y;
-}				t_pos;
-
-typedef struct	s_dir
-{
-	double			x;
-	double			y;
-}				t_dir;
-
-typedef struct	s_all
-{
-	t_mlx			mlx;
-	t_win			win;
-	t_img			img;
-	t_map			map;
-	t_pos			pos;
-	t_dir			dir;
-}				t_all;
+#endif //CUB3D_CUB3D_H
